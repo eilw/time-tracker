5588b242b57f69795d54974a7c0f334099b323a2
Eirik Wiig <Eirikwiig@gmail.com>
2016-12-02 16:50:54 +0000

    Check when employee have not reported an activity

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,11 +1,7 @@
 class Report
   include ActiveModel::Model
 
-  attr_accessor :employee, :client, :project
-
-  def employee_report
-    Activity.where(employee: employee)
-  end
+  attr_accessor :employee, :client, :project, :date_from, :date_to
 
   def client_report
     projects = Project.where(client: client)
@@ -14,5 +10,19 @@ class Report
 
   def project_report
     Activity.where(project: project)
+  end
+
+  def date_range
+    (Date.parse(date_from)..Date.parse(date_to)).to_a
+  end
+
+  def filled_in?(date)
+    activity_dates.include?(date)
+  end
+
+  private
+
+  def activity_dates
+    @_employee_dates ||= Activity.where(employee: employee, date: date_from..date_to).pluck(:date)
   end
 end
